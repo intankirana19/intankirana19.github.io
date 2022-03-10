e6f0fcf428c8a72315696eed951975cc466f3456
Intan Kirana <intankirana19@gmail.com>
2022-03-11 00:00:00 +0700

    kitten first test

---
+++ study-room/kitten/one.js
@@ -0,0 +1,9 @@
+// stupid one
+number = 21;
+convertedNumber = String("000" + number).slice(-3);
+console.log(convertedNumber);
+
+// cara dr google wkwk
+// number = 4;
+// convertedNumber = String(number).padStart(3, '0'); 
+// console.log(convertedNumber);
