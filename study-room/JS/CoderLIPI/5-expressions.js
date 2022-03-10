5e745390be08dfbd7d0fe89516fe329bfa913f43
Intan Kirana <intankirana19@gmail.com>
2022-03-11 00:00:26 +0700

    javascript expressions

---
+++ study-room/JS/CoderLIPI/5-expressions.js
@@ -0,0 +1,16 @@
+// a; // means nothing. Bad example 
+
+// // ~~~~~~~~~~~~~~~~~~~~~~~~ Types of expression ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// // 1. Variable Declaration
+// let a; // 'let' is the operator. a is the operand.
+
+// // 2. Assign a value
+// a = 4;
+
+// // 3. Perform an evaluation that returns a single value
+// b + c;
+
+let b = 3;
+let c = 2;
+let a = b + c; 
